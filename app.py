d7ccce670c1efd31c2e37e9c6b666ea6605ab672
Kenneth Ekandem <kennyekandem@gmail.com>
2026-06-24 02:27:04 +0100

    guess word for while loop

--- app.py
+++ app.py
@@ -101,3 +101,54 @@ def calculate(value_one, value_two, operator):
 
     
 calculate(value_one, value_two, operator)
+
+
+
+print("---------------- dictionary")
+
+monthConversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December"
+}
+
+print(monthConversions["Nov"])
+print(monthConversions.get("Love", "Not a valid key!"))
+
+i = 1
+
+while i <= 20:
+    print(i)
+    i+=1
+
+print("done with the loop")
+
+
+print("---------game time!")
+
+secret_word = "giraffe"
+guess = ""
+guess_count = 0
+guess_limit = 3
+out_of_guesses = False
+
+while guess != secret_word and not(out_of_guesses):
+    if guess_count < guess_limit:
+        guess = input("Enter your guess:")
+        guess_count += 1
+    else: 
+        out_of_guesses = True
+
+if out_of_guesses:
+    print("out of guesses -- lost!")
+else:
+    print("you win!")
