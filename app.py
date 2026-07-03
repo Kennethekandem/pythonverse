6a727860a6ecc535cf6493ce465aff7b8c086906
MAC <mac@MACs-MacBook-Pro.local>
2026-07-04 00:23:51 +0100

    add translator

--- app.py
+++ app.py
@@ -184,3 +184,20 @@ print(number_grid[0][2])
 for row in number_grid:
     for col in row:
         print(col)
+
+
+print("--------------global translator")
+
+def translate(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter.lower() in "aeiou":
+            if letter.isupper():
+                translation = translation+'G'
+            else:
+                translation = translation+'g'
+        else:
+            translation = translation+letter
+    return translation
+
+print(translate(input("enter a phrase:")))
