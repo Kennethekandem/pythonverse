445d114658c44faac579b15f9616398e95e05739
Kennethekandem <kennyekandem@gmail.com>
2026-07-09 22:09:58 +0100

    additions: purepath, file i/o, code cleanup

--- app.py
+++ app.py
@@ -1,231 +1,182 @@
+import useful_tools
+# print("   /|")
+# print("  / |")
+# print(" /  |")
+# print("/___|")
+# print("--------------------")
 
-print("   /|")
-print("  / |")
-print(" /  |")
-print("/___|")
-print("--------------------")
+# # -------------------
 
-# -------------------
+# character_name = "john"
+# age = 35
+# active = False
+# counted_string = 'random string'
+# squares = [1,4,9,16]
 
-character_name = "john"
-age = 35
-active = False
-counted_string = 'random string'
-squares = [1,4,9,16]
-
-if active:
-    print(f"my name is {character_name} and i am {age} years old")
-else:
-    print(f"unable to get that info\nplease, try again")
-
-
-
-print(len(counted_string))
-print(counted_string[3])
-print(squares)
-print(squares[2])
-print(squares[-1])
-
-print(f"hello, how old are you?")
-inputted = int(input())
+# if active:
+#     print(f"my name is {character_name} and i am {age} years old")
+# else:
+#     print(f"unable to get that info\nplease, try again")
 
 
-lucky_numbers = [4, 8, 15, 16, 24]
-friends = ["karen", "lucky", "max", "jon"]
-friends.extend(lucky_numbers)
 
-print(f"you are {inputted} years old")
-print(friends)
+# print(len(counted_string))
+# print(counted_string[3])
+# print(squares)
+# print(squares[2])
+# print(squares[-1])
 
-#----------------------
-# tuples
-
-coordinates = (4,2)
-print(coordinates)
+# print(f"hello, how old are you?")
+# inputted = int(input())
 
 
-def sayhi(value):
-    if(value <= 25):
-        print(f"hello, you are {value} years old? are you sure?")
-    else: 
-        print(f"man you are old. you mean you are {value} years old! 😳")
+# lucky_numbers = [4, 8, 15, 16, 24]
+# friends = ["karen", "lucky", "max", "jon"]
+# friends.extend(lucky_numbers)
 
-sayhi(inputted)
+# print(f"you are {inputted} years old")
+# print(friends)
 
-print("-------------------------")
+# #----------------------
+# # tuples
 
-def cube(num):
-    return num*num*num
-
-print(cube(3))
+# coordinates = (4,2)
+# print(coordinates)
 
 
-def max_num(num1, num2, num3):
-    if num1 >= num2 and num1 >= num3:
-        return num1
-    elif num2 >= num1 and num2 >= num2:
-        return num2
-    else:
-        return num3
+# def sayhi(value):
+#     if(value <= 25):
+#         print(f"hello, you are {value} years old? are you sure?")
+#     else: 
+#         print(f"man you are old. you mean you are {value} years old! 😳")
+
+# sayhi(inputted)
+
+# print("-------------------------")
+
+# def cube(num):
+#     return num*num*num
+
+# print(cube(3))
+
+
+# def max_num(num1, num2, num3):
+#     if num1 >= num2 and num1 >= num3:
+#         return num1
+#     elif num2 >= num1 and num2 >= num2:
+#         return num2
+#     else:
+#         return num3
     
-print(max_num(10, 40, 5))
-
-
-print("------------------------------- calculator")
-
-value_one = float(input("Input first number: "))
-operator = input("add operator: ")
-value_two = float(input("Input the second number: "))
-
-supported_operators = ["+", "-", "*", "/"]
-
-def check_operator(operator):
-    if operator not in supported_operators:
-        print("invalid operator")
-    else:
-        return True
-
-def calculate(value_one, value_two, operator):
-    # confirm_operator = check_operator(operator)
-    
-    if not check_operator(operator):
-        return None
-    if operator == supported_operators[0]:
-        print(value_one + value_two)
-    elif operator == supported_operators[1]:
-        print(value_one - value_two)
-    elif operator == supported_operators[2]:
-        print( value_one * value_two)
-    elif operator == supported_operators[3]:
-        print(value_one / value_two)
-    
-
-    
-calculate(value_one, value_two, operator)
+# print(max_num(10, 40, 5))
 
 
 
-print("---------------- dictionary")
+# print("---------------- dictionary")
 
-monthConversions = {
-    "Jan": "January",
-    "Feb": "February",
-    "Mar": "March",
-    "Apr": "April",
-    "May": "May",
-    "Jun": "June",
-    "Jul": "July",
-    "Aug": "August",
-    "Sep": "September",
-    "Oct": "October",
-    "Nov": "November",
-    "Dec": "December"
-}
+# monthConversions = {
+#     "Jan": "January",
+#     "Feb": "February",
+#     "Mar": "March",
+#     "Apr": "April",
+#     "May": "May",
+#     "Jun": "June",
+#     "Jul": "July",
+#     "Aug": "August",
+#     "Sep": "September",
+#     "Oct": "October",
+#     "Nov": "November",
+#     "Dec": "December"
+# }
 
-print(monthConversions["Nov"])
-print(monthConversions.get("Love", "Not a valid key!"))
+# print(monthConversions["Nov"])
+# print(monthConversions.get("Love", "Not a valid key!"))
 
-i = 1
+# i = 1
 
-while i <= 20:
-    print(i)
-    i+=1
+# while i <= 20:
+#     print(i)
+#     i+=1
 
-print("done with the loop")
-
-
-print("---------game time!")
-
-secret_word = "giraffe"
-guess = ""
-guess_count = 0
-guess_limit = 3
-out_of_guesses = False
-
-while guess != secret_word and not(out_of_guesses):
-    if guess_count < guess_limit:
-        guess = input("Enter your guess:")
-        guess_count += 1
-    else: 
-        out_of_guesses = True
-
-if out_of_guesses:
-    print("out of guesses -- lost!")
-else:
-    print("you win!")
-
-print("-----------------------for loops")
-
-letters = ["Forbidden", "Hate", "Redemption"]
-
-# for letter in letters:
-#     print(letter)
-
-for index in range(len(letters)):
-    print(letters[index])
-
-def raise_to_power(base_num, pow_num):
-    result = 1
-    for i in range(pow_num):
-        result = result * base_num
-    return result
-
-print(f"it is raised to -- {raise_to_power(3, 4)}")
-
-print("----------nested for loop & 2D grid")
-number_grid = [
-    [1, 2, 2, 4],
-    [5, 6, 7, 8],
-    [9, 9, 9],
-    [0]
-]
-
-print(number_grid[0][2])
-
-for row in number_grid:
-    for col in row:
-        print(col)
+# print("done with the loop")
 
 
-print("--------------global translator ----")
+# print("---------game time!")
 
-def translate(phrase):
-    translation = ""
-    for letter in phrase:
-        if letter.lower() in "aeiou":
-            if letter.isupper():
-                translation = translation+'G'
-            else:
-                translation = translation+'g'
-        else:
-            translation = translation+letter
-    return translation
+# secret_word = "giraffe"
+# guess = ""
+# guess_count = 0
+# guess_limit = 3
+# out_of_guesses = False
 
-print(translate(input("enter a phrase:")))
+# while guess != secret_word and not(out_of_guesses):
+#     if guess_count < guess_limit:
+#         guess = input("Enter your guess:")
+#         guess_count += 1
+#     else: 
+#         out_of_guesses = True
+
+# if out_of_guesses:
+#     print("out of guesses -- lost!")
+# else:
+#     print("you win!")
+
+# print("-----------------------for loops")
+
+# letters = ["Forbidden", "Hate", "Redemption"]
+
+# # for letter in letters:
+# #     print(letter)
+
+# for index in range(len(letters)):
+#     print(letters[index])
+
+# def raise_to_power(base_num, pow_num):
+#     result = 1
+#     for i in range(pow_num):
+#         result = result * base_num
+#     return result
+
+# print(f"it is raised to -- {raise_to_power(3, 4)}")
+
+# print("----------nested for loop & 2D grid")
+# number_grid = [
+#     [1, 2, 2, 4],
+#     [5, 6, 7, 8],
+#     [9, 9, 9],
+#     [0]
+# ]
+
+# print(number_grid[0][2])
+
+# for row in number_grid:
+#     for col in row:
+#         print(col)
 
 
-print("--------- try/catch")
-
-try:
-    print(int(input("enter a number: ")))
-except ZeroDivisionError as err: 
-    print(f"Error: {err}")
-except ValueError:
-    print("invalid input")
 
 
-print("--------------Read file")
 
-employee_file = open("employees.txt", "r")
+# print("--------- try/catch")
 
-# print(employee_file.readable())
+# try:
+#     print(int(input("enter a number: ")))
+# except ZeroDivisionError as err: 
+#     print(f"Error: {err}")
+# except ValueError:
+#     print("invalid input")
 
-if employee_file.readable():
-    print(True)
-else:
-    print('not readable')
 
-for employee in employee_file.readlines():
-    print(employee)
+text = "my cat is from jamaica and the cat used wore a cap from jamaica all the time till my mum removed it. cat watching is cool"
 
-employee_file.close()
+text.lower()
+words = text.split()
+
+counts = {}
+
+for word in words:
+    counts[word] = counts.get(word, 0) + 1
+print(counts)
+
+
+print(useful_tools.roll_dice(int(input("add a number: "))))
---
+++ file_io_write.py
@@ -0,0 +1,3 @@
+employees_file = open("index.html", 'w')
+employees_file.write("<p>Hello World</p>")
+employees_file.close()
